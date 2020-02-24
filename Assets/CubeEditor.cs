5109da319cc5cbd0f422d33449a91bbaa164fed3
Michael Numrich <mnumrich@gmail.com>
2020-02-24 13:16:35 -0700

    Added cube labels

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
 {
     [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
-    
+
+    TextMesh textMesh;
+
     void Update()
     {
         Vector3 snapPos;
+
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+        
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPos.x / gridSize + ", " + snapPos.z / gridSize;
 
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
     }

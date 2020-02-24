133d29cbdcf225d2e4eb7a0963e256aaa704b3fd
Michael Numrich <mnumrich@gmail.com>
2020-02-24 16:31:55 -0700

    Made enemy move

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -19,7 +19,9 @@ public class CubeEditor : MonoBehaviour
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
         
         textMesh = GetComponentInChildren<TextMesh>();
-        textMesh.text = snapPos.x / gridSize + ", " + snapPos.z / gridSize;
+        string labelText = snapPos.x / gridSize + ", " + snapPos.z / gridSize;
+        textMesh.text = labelText;
+        gameObject.name = "Cube [" + labelText + "]";
 
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
     }
---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FollowPath());
+    }
+
+    IEnumerator FollowPath()
+    {
+        print("Starting Patrol...");
+        foreach(Waypoint waypoint in path)
+        {
+            transform.position = waypoint.transform.position;
+            print("Visiting block: " + waypoint.name);
+            yield return new WaitForSeconds(1f);
+        }    
+        print("Ending patrol");
+    }
+}

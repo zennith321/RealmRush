1928e4418142148a1a985cfb8f561c26424d659a
Michael Numrich <mnumrich1@gmail.com>
2020-02-26 20:39:54 -0700

    Created datastructure for pathfinder

---
+++ Assets/Pathfinder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadBlocks();
+    }
+
+    private void LoadBlocks()
+    {
+        var waypoints = FindObjectsOfType<Waypoint>();
+        foreach (Waypoint waypoint in waypoints)
+        {
+            var gridPos = waypoint.GetGridPos();
+            bool isOverlapping = grid.ContainsKey(gridPos);
+            if (isOverlapping)
+            {
+                Debug.LogWarning("Skipping overlapping block " + waypoint);
+            }
+            else
+            {
+                grid.Add(gridPos, waypoint);
+            }
+        }
+        print("Loaded " + grid.Count + " blocks");
+    }
+}

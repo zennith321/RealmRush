db19358f5b1dec7d04af6bcf9916ff62b1166012
Michael Numrich <mnumrich@gmail.com>
2020-03-01 20:02:14 -0700

    Made tower shoot, enemies die

---
+++ Assets/Scripts/EnemyCollisionHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCollisionHandler : MonoBehaviour
+{
+    [SerializeField] int hitPoints = 10;
+    [SerializeField] GameObject hitFX = null;
+    [SerializeField] Transform parent;
+
+    void Start()
+    {
+        AddNonTriggerBoxCollider();
+    }
+
+    private void AddNonTriggerBoxCollider()
+    {
+        Collider nonTriggerBoxCollider = gameObject.AddComponent<BoxCollider>();
+        nonTriggerBoxCollider.isTrigger = false;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        if (hitPoints <= 0)
+        {
+            KillEnemy();
+        }
+    }
+
+    private void ProcessHit()
+    {
+        hitPoints--;
+        Instantiate(hitFX, transform.position, Quaternion.identity, parent);
+    }
+
+    private void KillEnemy()
+    {
+        Destroy(gameObject);
+    }
+}

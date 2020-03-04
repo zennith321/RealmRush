8c62e81c61a5bdd9ab2416c07e3fab9be2172ce6
Michael Numrich <mnumrich@gmail.com>
2020-03-04 08:58:09 -0700

    Added player health functionality

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] int playerHitPoints = 10;
+    [SerializeField] Text healthText;
+
+    void Start()
+    {
+        healthText.text = playerHitPoints.ToString();
+    }
 
     public void PlayerHit()
     {
-        print("enemy reached end");
         playerHitPoints--;
+        healthText.text = playerHitPoints.ToString();
     }
 }
